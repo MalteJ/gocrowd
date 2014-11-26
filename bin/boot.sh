a3700d9391270c3fa730472fc10609a6e1700a66
Malte Janduda <mail@janduda.net>
2014-11-26 01:28:09 +0100

    fix

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,16 +2,16 @@ set -e
 set -x
 export APP_ROOT=/app
 
-if [ -f $APP_ROOT/gocrowd/htdocs/gocrowd.json ]
-then
-  mv $APP_ROOT/gocrowd/htdocs/gocrowd.json $APP_ROOT/gocrowd/gocrowd.json
-fi
+#if [ -f $APP_ROOT/gocrowd/htdocs/gocrowd.json ]
+#then
+#  mv $APP_ROOT/gocrowd/htdocs/gocrowd.json $APP_ROOT/gocrowd/gocrowd.json
+#fi
 
-touch $APP_ROOT/gocrowd/logs/access.log
-touch $APP_ROOT/gocrowd/logs/error.log
+#touch $APP_ROOT/gocrowd/logs/access.log
+#touch $APP_ROOT/gocrowd/logs/error.log
 
 echo "My outgoing IP to Crowd:" `curl ifconfig.co 2> /dev/null`
 
-(tail -f -n 0 $APP_ROOT/gocrowd/logs/*log &)
+#(tail -f -n 0 $APP_ROOT/gocrowd/logs/*log &)
 cd $APP_ROOT/gocrowd
 exec $APP_ROOT/gocrowd/gocrowd
