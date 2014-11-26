9ffea7c43b68d8716d518b4adbe5683d987ba3cb
Malte Janduda <mail@janduda.net>
2014-11-26 01:57:21 +0100

    fix

--- src/gocrowd.go
+++ src/gocrowd.go
@@ -2,12 +2,26 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.RequestURI)
+    f := r.RequestURI
+    if f[0] == "/" {
+        f = f[1:]
+    }
+    
+    file_content, err := ioutil.ReadFile(r.RequestURI)
+    if err != nil {
+        log.Error(err)
+    }
+
+    err := w.Write(file_content)
+    if err != nil {
+        log.Error(err)
+    }
 }
 
 func main() {
