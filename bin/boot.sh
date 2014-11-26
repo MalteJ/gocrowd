fb9837cfb4d9ef847e2527afe1b3a983db7fe260
Malte Janduda <mail@janduda.net>
2014-11-26 01:45:12 +0100

    fix

--- bin/boot.sh
+++ bin/boot.sh
@@ -5,11 +5,7 @@ export APP_ROOT=`pwd`
 #  mv $APP_ROOT/gocrowd/htdocs/gocrowd.json $APP_ROOT/gocrowd/gocrowd.json
 #fi
 
-#touch $APP_ROOT/gocrowd/logs/access.log
-#touch $APP_ROOT/gocrowd/logs/error.log
-
 echo "My outgoing IP to Crowd:" `curl ifconfig.co 2> /dev/null`
 
-#(tail -f -n 0 $APP_ROOT/gocrowd/logs/*log &)
-#cd $APP_ROOT/gocrowd
+cd $APP_ROOT/gocrowd
 exec $APP_ROOT/gocrowd/gocrowd
